93fa6d65bb024f9fa4275db908d94153e5d68b6d
Sky Chan <diufanshu@gmail.com>
2017-02-05 22:41:43 +0800

    Add Provider Generator

--- src/RL.java
+++ src/RL.java
@@ -1,7 +1,28 @@
-import java.io.IOException;
+import ilog.concert.*;
+import ilog.cp.*;
+import java.io.*;
+import java.util.*;
 
 public class RL {
+	
+	static class IntervalVarList extends ArrayList<IloIntervalVar> {
+        public IloIntervalVar[] toArray() {
+            return (IloIntervalVar[]) this.toArray(new IloIntervalVar[this.size()]);
+        }
+    }
+
+    static IloIntExpr[] arrayFromList(List<IloIntExpr> list) {
+        return (IloIntExpr[]) list.toArray(new IloIntExpr[list.size()]);
+    }
+	
 	public static void solve(String filename) throws IOException {
-		System.out.println(filename);
+//		System.out.println(filename);
+		int nbTasks, nbRenewable, nbNonRenewable;
+		DataReader data = new DataReader(filename);
+		
+		 nbTasks = data.next();
+         nbRenewable = data.next();
+         nbNonRenewable = data.next();
+		
 	}
 }
--- src/Solver.java
+++ src/Solver.java
@@ -4,8 +4,8 @@ public class Solver {
 
 	public static void main(String[] args) {
 		try {
-			SchedRCPSPMM.solve("data/m12_5.mm");
-//			RL.solve("data/m12_5.mm");
+//			SchedRCPSPMM.solve("data/test");
+			RL.solve("data/test");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
