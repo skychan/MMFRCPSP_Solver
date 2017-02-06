329376a966b96377e2f2767f28e7066633ad9f35
Sky Chan <diufanshu@gmail.com>
2017-02-06 08:46:56 +0800

    add new class

--- src/ProviderGenerator.java
+++ src/ProviderGenerator.java
@@ -1,4 +1,31 @@
+import java.io.IOException;
 
 public class ProviderGenerator {
+	public static void main(String[] args) {
+		
+	}
 	
+	public static void generate(String filename) throws IOException {
+		int nbTasks, nbRenewable, nbNonRenewable;
+		DataReader data = new DataReader(filename);
+		try {
+			
+	    	nbTasks = data.next();
+		    nbRenewable = data.next();
+		    nbNonRenewable = data.next();
+		    
+		    
+		    
+		} catch (IOException e) {
+			// TODO: handle exception
+			System.err.println("Error: " + e);
+		}
+		
+		 
+	}
+	
+	public static void generatemap() {
+		System.out.println("1");
+//		return ;
+	}
 }
